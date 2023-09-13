8a283cdc7da86563f92ea0b2a2056cc20a7f8809
salauddin96 <salauddin33-5088@diu.edu.bd>
2023-09-14 01:37:57 +0600

    initial version

---
+++ app/src/main/java/org/demo/project01_android/login.kt
@@ -0,0 +1,11 @@
+package org.demo.project01_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
